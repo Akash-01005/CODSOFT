b3a101cd1c32383a01d42612caf25e7c9c80f332
WebDevKraken <webdevepak@gmail.com>
2024-01-21 10:26:09 +0530

    1st Commit

---
+++ Calculator/app.js
@@ -0,0 +1,18 @@
+let disp=document.getElementById("display");
+function clr(){
+    disp.value=" ";
+}
+function cal(){
+    try{
+        disp.value=eval(disp.value);
+    }
+    catch(exception){
+        disp.value="Invalid";
+    }
+}
+function del(){
+    disp.value=disp.value.slice(0,-1);
+}
+function show(n){
+    disp.value+=n;
+}
